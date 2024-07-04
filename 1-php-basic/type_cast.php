037c7ae4fee3d4f2e4924e73db5226416500878c
Min Nyan Htaw <153126638+minnyanhtaw@users.noreply.github.com>
2024-07-04 19:00:00 +0700

    Day 6 Type Decleration

--- 1-php-basic/type_cast.php
+++ 1-php-basic/type_cast.php
@@ -15,4 +15,34 @@ $car = (object) [
 
 // var_dump($car);
 // print($car['model']);
-print($car->model);
+// print($car->model);
+
+
+function area(int $w, int $h): int
+{
+    return $w * $h;
+}
+
+// print(area("a", "b"));
+// print(area(4, 5));
+
+// $number = ["a", 4, 5, 6, 4];
+$number = [3, 4, 5, 6, 4];
+
+
+function processArray(array $number): int
+{
+    return array_sum($number);
+}
+// print(processArray($number));
+
+function performOperation(int $a, int $b, callable $operations): int
+{
+    return $operations($a, $b);
+}
+
+$additionCallback = function (int $x, int $y): int {
+    return $x + $y;
+};
+
+print(performOperation(5, 4, $additionCallback));
