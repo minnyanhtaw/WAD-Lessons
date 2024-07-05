aad31e0e988b7cb2ce0fa90f75323b4aa8c61155
Min Nyan Htaw <153126638+minnyanhtaw@users.noreply.github.com>
2024-07-05 20:55:30 +0700

    Day 6 request-response

---
+++ 1-php-basic/request-response/save.php
@@ -0,0 +1,11 @@
+<?php
+echo "<pre>";
+print_r($_GET);
+
+// echo "Your name is " . $_GET["your_name"] . " and you was born in " . $_GET["bd"];
+
+print_r($_POST);
+
+echo "Your name is " . $_POST["your_name"] . " and you was born in " . $_POST["bd"];
+
+print_r($_FILES);
---
+++ 1-php-basic/request-response/test.php
@@ -0,0 +1,20 @@
+<?php
+
+// echo "a";
+// echo "\n";
+// die("it is finish");
+// echo "b";
+
+// echo uniqid();
+// echo "\n";
+
+// echo uniqid();
+// echo "\n";
+
+// echo uniqid();
+// echo "\n";
+
+// echo uniqid();
+// echo "\n";
+
+echo mb_substr("hello hello", 0, 5);
