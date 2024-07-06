9287b9919197f95ecd68f47b8ceccc382d58aefa
Min Nyan Htaw <153126638+minnyanhtaw@users.noreply.github.com>
2024-07-07 00:54:40 +0700

    Show Area Record-List

--- 1-php-basic/projects/record-list.php
+++ 1-php-basic/projects/record-list.php
@@ -1,18 +1,3 @@
-<?php
-// print_r($_POST);
-// var_dump(empty($_POST["home_width"]));
-// var_dump(empty($_POST["home_breadth"]));
-// var_dump(empty($_POST["calc_btn"]));
-
-// die("________it is finish ________");
-
-if (empty($_POST["home_width"]) || empty($_POST["home_breadth"])) {
-    // die("need all input");
-    header("Location:index.php");
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -88,45 +73,43 @@ if (empty($_POST["home_width"]) || empty($_POST["home_breadth"])) {
                     </svg>
                 </li>
                 <li class="inline-flex items-center">
-                    <a class="flex items-center text-black font-bold text-sm  hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500" href="#">
+                    <a class="flex items-center text-gray-500 font-bold text-sm  hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500" href="#">
                         Area Calculator
+                    </a>
+                    <svg class="flex-shrink-0 mx-2 overflow-visible size-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="m9 18 6-6-6-6"></path>
+                    </svg>
                     </a>
                 </li>
 
+                <li class="inline-flex items-center">
+                    <a class="flex items-center text-black font-bold text-sm  hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500" href="#">
+                        Calculate Results
+                    </a>
+                </li>
             </ol>
             <hr class=" border-white my-3">
 
             <?php
-
-
-            $width = $_POST["home_width"];
-            $breadth = $_POST["home_breadth"];
-            $area = $width * $breadth;
-
             $fileName = "saveRecord.txt";
             if (!file_exists($fileName)) {
                 touch($fileName);
             }
-            $fileStream = fopen($fileName, "a");
-            fwrite($fileStream, "\n$width * $breadth = $area");
-            fclose($fileStream);
-
+            $fileStream = fopen($fileName, "r");
+            while (!feof($fileStream)) :
+                $content = fgets($fileStream);
+                if ($content === "\n") {
+                    continue;
+                }
             ?>
-            <p class=" text-center text-5xl mb-5">
-                <?= $area ?> Sqft
-            </p>
+                <p class=" bg-white p-2 mb-3 rounded font-mono">
+                    <?= $content ?> Sqft
+                </p>
+            <?php endwhile; ?>
 
-            <div class=" flex flex-col gap-4">
-                <a href="./index.php" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none w-full justify-center">
-                    Calculate Again
-                </a>
-
-                <a href="./record-list.php" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-gray-200 text-gray-500 hover:border-blue-600 hover:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500 dark:hover:border-blue-600 w-full justify-center">
-                    All Records
-                </a>
-            </div>
-
-        </div>
+            <a href="./index.php" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none w-full justify-center">
+                Calculate Again
+            </a>
 
         </div>
 
--- 1-php-basic/projects/test.php
+++ 1-php-basic/projects/test.php
@@ -17,18 +17,39 @@
 // rmdir("mytest");
 
 $fileName = "my.txt";
-if (file_exists($fileName)) {
-    touch($fileName);
+$fileStream = fopen($fileName, "r");
+
+// echo fread($fileStream, 1024);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+
+// echo fgets($fileStream);
+// echo fgets($fileStream);
+// echo fgets($fileStream);
+// echo fgets($fileStream);
+
+while (!feof($fileStream)) {
+    echo fgets($fileStream);
 }
 
-$fileStream = fopen($fileName, "w");
-fwrite($fileStream, "Min");
 fclose($fileStream);
+// if (file_exists($fileName)) {
+//     touch($fileName);
+// }
 
-$fileStream = fopen($fileName, "w");
-fwrite($fileStream, "Nyan ");
-fclose($fileStream);
+// $fileStream = fopen($fileName, "w");
+// fwrite($fileStream, "Min");
+// fclose($fileStream);
 
-$fileStream = fopen($fileName, "w");
-fwrite($fileStream, "Htaw ");
-fclose($fileStream);
+// $fileStream = fopen($fileName, "w");
+// fwrite($fileStream, "Nyan ");
+// fclose($fileStream);
+
+// $fileStream = fopen($fileName, "w");
+// fwrite($fileStream, "Htaw ");
+// fclose($fileStream);
