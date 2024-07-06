ae4023579f516df6f2ecef0f371ec57b62beafdf
Min Nyan Htaw <153126638+minnyanhtaw@users.noreply.github.com>
2024-07-06 22:18:28 +0700

    Basic Ui of Area Calculator project

---
+++ 1-php-basic/projects/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./*.php", "node_modules/preline/dist/*.js"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("preline/plugin")],
+};
