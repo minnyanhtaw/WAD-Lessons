a13b9b7e17256a53406d146d3ec3393e55768a27
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-10-05 01:42:31 +0700

    PHP Day 8 / split sidebar file and combine file

--- 1-php-basic/project-2nd/area.php
+++ 1-php-basic/project-2nd/area.php
@@ -33,30 +33,7 @@ if (empty($_POST["home_width"]) || empty($_POST["home_breadth"])) {
             <h1 class=" text-2xl font-bold">Backend Project</h1>
         </div>
 
-        <aside id="default-sidebar"
-            class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-            aria-label="Sidebar">
-            <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                <ul class="space-y-2 font-medium">
-                    <li class=" text-xl font-bold">
-                        My App
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
-                            </svg>
-
-                            <span class="flex-1 ms-3 whitespace-nowrap">Area Calculator</span>
-
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </aside>
+        <?php include("./sidebar.php") ?>
 
         <section class="px-4 pb-4 pt-2 sm:ml-64">
             <header class=" hidden md:block lg:block mb-5">
